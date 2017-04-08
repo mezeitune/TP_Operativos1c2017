fc2d9ff38b0cb54eb461647c3f94c6590279efae
mdasilvaevora <manudse10@gmail.com>
2017-04-08 18:04:02 -0300

    Fix Kernel servidor ; FS cliente

--- File System/src/File System.c
+++ File System/src/File System.c
@@ -22,7 +22,7 @@
 #include <commons/config.h>
 
 
-t_config* configuracion_fileSys;
+t_config* configuracion_FS;
 char *ipKernel;
 char *puerto;
 char *puntoMontaje;
@@ -39,7 +39,6 @@ int main(void){
 	char orden;
 	//leerConfiguracion("/home/utnso/workspace/tp-2017-1c-servomotor/\"File System\"/config_FileSys");
 
-<<<<<<< HEAD
 	int socket_Kernel = crear_socket_cliente("127.0.0.1","5002"); //Variable definidas
 		while(1)
 			{
@@ -52,10 +51,9 @@ int main(void){
 int crear_socket_cliente(char * ip, char * puerto){
     int descriptorArchivo, estado;
     struct addrinfo hints, *infoServer, *n;
-=======
+
 	leerConfiguracion("/home/utnso/workspace/tp-2017-1c-servomotor/\"File System\"/config_FileSys");
 	//printf("CONFIGURACIONES\nPuerto=%s\nPunto Montaje=%s\n",puerto,puntoMontaje);
->>>>>>> d8b580b27910d6e730ad9065f8dbfc1223c770ef
 
     memset(&hints,0,sizeof (struct addrinfo));
     hints.ai_family = AF_UNSPEC;
@@ -80,16 +78,15 @@ int crear_socket_cliente(char * ip, char * puerto){
 
     estado = connect(descriptorArchivo, n->ai_addr, n->ai_addrlen);
 
-<<<<<<< HEAD
+
     if (estado == -1){
         perror("Error conectando el socket");
         freeaddrinfo(infoServer);
         return -1;
     }
-=======
-	puerto = config_get_string_value(configuracion_kernel,"PUERTO");
-	puntoMontaje = config_get_string_value(configuracion_kernel,"PUNTO_MONTAJE");
->>>>>>> d8b580b27910d6e730ad9065f8dbfc1223c770ef
+
+	puerto = config_get_string_value(configuracion_FS,"PUERTO");
+	puntoMontaje = config_get_string_value(configuracion_FS,"PUNTO_MONTAJE");
 
     freeaddrinfo(infoServer);
 
@@ -108,9 +105,9 @@ void enviar(int socket, void* cosaAEnviar, int tamanio){
 }
 void leerConfiguracion(char* ruta){
 
-	puerto = config_get_string_value(configuracion_fileSys,"PUERTO_KERNEL");
-	ipKernel= config_get_string_value(configuracion_fileSys, "IP_KERNEL");
-	puntoMontaje = config_get_string_value(configuracion_fileSys,"PUNTO_MONTAJE");
+	puerto = config_get_string_value(configuracion_FS,"PUERTO_KERNEL");
+	ipKernel= config_get_string_value(configuracion_FS, "IP_KERNEL");
+	puntoMontaje = config_get_string_value(configuracion_FS,"PUNTO_MONTAJE");
 
 }
 
