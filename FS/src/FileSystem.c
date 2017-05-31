722da2a23e9977e38abe5a3a36360ddf28357de7
mezeitune <matizeitune@gmail.com>
2017-05-31 17:16:34 -0300

    libreria FS

--- FS/src/FileSystem.c
+++ FS/src/FileSystem.c
@@ -26,6 +26,7 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include "conexiones.h"
+#include "permisos.h"
 #include <commons/log.h>
 #include <commons/bitarray.h>
 
@@ -339,59 +340,6 @@ int recibirConexion(int socket_servidor){
 }
 
 
-void printFilePermissions(char* archivo){
-
-    struct stat fileStat;
-    stat(archivo,&fileStat);
-
-
-    printf("Information for %s\n",archivo);
-    printf("---------------------------\n");
-    printf("File Size: \t\t%d bytes\n",fileStat.st_size);
-    printf("Number of Links: \t%d\n",fileStat.st_nlink);
-    printf("File inode: \t\t%d\n",fileStat.st_ino);
-
-    printf("File Permissions: \t");
-    printf( (S_ISDIR(fileStat.st_mode)) ? "d" : "-");
-    printf( (fileStat.st_mode & S_IRUSR) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWUSR) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXUSR) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IRGRP) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWGRP) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXGRP) ? "x" : "-");
-    printf( (fileStat.st_mode & S_IROTH) ? "r" : "-");
-    printf( (fileStat.st_mode & S_IWOTH) ? "w" : "-");
-    printf( (fileStat.st_mode & S_IXOTH) ? "x" : "-");
-}
-
-int archivoEnModoEscritura(char* archivo){
-	//Aca en realidad lo que tengo que hacer es recibir del Kernel
-	//si esta en modo Escritura ese archivo en la tabla de archivos por proceso
-	//pero por ahora queda asi para no andar metiendo sockets de por medio
-	 struct stat fileStat;
-	    stat(archivo,&fileStat);
-
-
-	   if(fileStat.st_mode & S_IWGRP){
-		   return 1 ;
-	   }else{
-		   return 0;
-	   }
-}
-
-int archivoEnModoLectura(char *archivo){
-	 struct stat fileStat;
-	    stat(archivo,&fileStat);
-	        //return 1;
-
-	    if(fileStat.st_mode & S_IROTH){
-	 		   return 1 ;
-	 	   }else{
-	 		   return 0;
-	 	   }
-
-
-}
 
 
 
